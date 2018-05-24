c4021b396769e173976a504b41e62f053db481b3
burkaans <raimonds.buholts@gmail.com>
2018-05-24 21:32:44 +0300

    java_day_3
    last commit

--- Java_day_3/src/java_day_3.java
+++ Java_day_3/src/java_day_3.java
@@ -21,6 +21,26 @@ public class java_day_3 {
 
 		System.out.println(str);
 		
+		
+		System.out.println(str.toUpperCase());
+		System.out.println(str.toLowerCase());
+		System.out.println(str);
+		
+		System.out.println(str.substring(5));  //nogriezh pirmos 5 simbolus
+		System.out.println(str.substring(5, 9));  // izgriezh un izvada simbolus starp 5. un 9. simbolu
+		System.out.println(str.substring(5, str.length())); // izgriezh un izvada simbolus saakot no 5. liidz beigaam.
+		System.out.println(str.substring(5, str.length()-10)); // izgriezh un izvada simbolus saakot no 5. 
+		//liidz beigaam + nogriezh arii peedeejos 10 simbolus no beigaam.
+		System.out.println("teksts sastaav no: " + str.length()+ " simboliem."); // paraada kopeejo stringag garumu
+		
+		System.out.println(str.contains("kruts")); //boolean vai stringaa ir "kruts"
+		System.out.println(str.contains("KRUTS")); // burtu lielums ir svariigs
+		System.out.println("kruts".equalsIgnoreCase("KRUTS")); // saliidzina stringus lai nenjem veeraa lielos burtus
+		
+
+
+
+
 	
 
 	}
